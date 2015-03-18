17a9c9e801581750c2603ef920d14f5beabf6ddd
Guitton Yann <yann.guitton@univ-nantes.fr>
2015-03-18 17:30:15 +0100

    Update dialog.mehalocoa.cldetect.R
    add "" around MeHalocoaenv

--- R/dialog.mehalocoa.cldetect.R
+++ R/dialog.mehalocoa.cldetect.R
@@ -152,12 +152,12 @@ if(method==""){
         tkdestroy(tt)
            
         #test if xsAnnotate object exists in MeHaloCoAenv or .GlobalEnv
-        if(length(which(ls(MeHaloCoAenv)==tclvalue(xsAnnotatevar)))==0){
+        if(length(which(ls("MeHaloCoAenv")==tclvalue(xsAnnotatevar)))==0){
             an<-get(ls(.GlobalEnv)[which(ls(.GlobalEnv)==tclvalue(xsAnnotatevar))], envir=as.environment(.GlobalEnv))
             print("Note: Use of an xsAnnotate object from GlobalEnv as entry")
         }else
         {
-            an<-get(ls(MeHaloCoAenv)[which(ls(MeHaloCoAenv)==tclvalue(xsAnnotatevar))], envir=as.environment(MeHaloCoAenv))
+            an<-get(ls("MeHaloCoAenv")[which(ls("MeHaloCoAenv")==tclvalue(xsAnnotatevar))], envir=as.environment("MeHaloCoAenv"))
         }
 
         if((class(an)=="xsAnnotate")==FALSE){
@@ -222,7 +222,7 @@ if(method==""){
           myfile2=tclvalue(myfile2var),
           m1need=tclvalue(m1needvar)
         )
-        )), envir=as.environment(MeHaloCoAenv))
+        )), envir=as.environment("MeHaloCoAenv"))
       dev.off()
     }
                 
@@ -446,7 +446,7 @@ if(method=="scan"){
     
     if (length(samples)>1){  
       print("several files")
-       assign("matcl",vector(mode="list", length=length(samples)), envir=as.environment(MeHaloCoAenv))
+       assign("matcl",vector(mode="list", length=length(samples)), envir=as.environment("MeHaloCoAenv"))
        
      
       projectpdf(dir=outdir,projectName="IFC", ext="pdf")
@@ -500,7 +500,7 @@ if(method=="scan"){
           myfile2=tclvalue(myfile2var),
           m1need=tclvalue(m1needvar)
         )
-        )), envir=as.environment(MeHaloCoAenv))
+        )), envir=as.environment("MeHaloCoAenv"))
       dev.off()
     }
                 
